456d387cab6cd6ac8558e93a3ec5206a88dd6726
Tim Junell <tkpjun@outlook.com>
2015-09-30 20:28:13 +0300

    Very simplistic battle simulation and ladder updating.

---
+++ Assets/Scripts/Participant.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Participant : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void challengeOne(List<Participant> all, Action<int> callback)
+    {
+        //TODO
+        callback(0);
+    }
+}
--- Assets/Scripts/Tournament.cs
+++ Assets/Scripts/Tournament.cs
@@ -1,31 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
 
 public class Tournament : MonoBehaviour {
 
     public GameObject cell;
     GameObject canvas;
+    List<Participant> participants;
+    List<GameObject> ladder;
+    int nextTurn;
+    float timeUntilNext;
+    bool waiting;
+
 	// Use this for initialization
 	void Start () {
         canvas = GameObject.Find("Canvas");
-        Create(16);
+        ladder = Create(16);
+        participants = ladder.Select(x => x.GetComponent<Participant>()).Reverse().ToList();
+        nextTurn = 0;
+        timeUntilNext = 0;
+        waiting = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (waiting == false && timeUntilNext <= 0) {
+            var challengerPos = ladder.FindIndex(x => x.GetComponent<Participant>() == participants[nextTurn]);
+            var ordered = ladder.Select(x => x.GetComponent<Participant>()).ToList();
+            participants[nextTurn].challengeOne(ordered, 
+                (int challengeePos) => {
+                    SetupMatch(challengerPos, challengeePos);
+                }
+            );
+            waiting = true;
+            IncrementNext();
+        }
+        else {
+            timeUntilNext -= Time.deltaTime;
+        }
 	}
 
-    void Create(int player_amount)
+    void IncrementNext()
     {
+        nextTurn = (nextTurn + 1) % participants.Count;
+    }
+
+    void SetupMatch(int challenger, int challengee)
+    {
+        //TODO
+        ladder[challenger].transform.DOScale(1.2f, 1);
+        ladder[challengee].transform.DOScale(1.2f, 1).OnComplete(() =>
+        {
+            ladder[challenger].transform.DOScale(1f, 1);
+            ladder[challengee].transform.DOScale(1f, 1).OnComplete(() =>
+            {
+                timeUntilNext = 1;
+                waiting = false;
+            });
+            HandleChallengerVictory(challenger, challengee);
+        });
+    }
+
+    List<GameObject> Create(int player_amount)
+    {
+        var list = new List<GameObject>();
         var next_y = -30 + player_amount / 2 * 64;
         for (var i = 1; i <= player_amount; i++)
         {
             var obj = (GameObject)Instantiate(cell, Vector3.zero, Quaternion.identity);
             obj.transform.SetParent(canvas.transform, false);
+            obj.transform.GetComponentsInChildren<Text>()[0].text = "Player " + (player_amount + 1 - i).ToString();
             obj.transform.GetComponentsInChildren<Text>()[1].text = i.ToString();
             obj.transform.position = new Vector3(0, next_y, 0);
+            list.Add(obj);
+
+            next_y -= 64;
+        }
+        return list;
+    }
+
+    void HandleChallengerVictory(int winnerPos, int loserPos)
+    {
+        if (winnerPos > loserPos)
+        {
+            var p = ladder[winnerPos];
+            ladder.RemoveAt(winnerPos);
+            ladder.Insert(loserPos, p);
+            RepositionCells();
+        }
+    }
+
+    void RepositionCells()
+    {
+        var next_y = -30 + ladder.Count / 2 * 64;
+        for (var i = 1; i <= ladder.Count; i++)
+        {
+            var p = ladder[i - 1];
+            p.transform.GetComponentsInChildren<Text>()[1].text = i.ToString();
+            p.transform.position = new Vector3(0, next_y, 0);
 
             next_y -= 64;
         }
