0207ed360f359ed8a6ab22ec29bd5698130bf002
Anton <antonbjo@gmail.com>
2015-10-01 18:03:23 +0300

    create match class for a battle of RPS

---
+++ Assets/Scripts/RPS/Match.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public class Match {
+
+	private IRPSPlayer player1;
+	private IRPSPlayer player2;
+	
+
+	public Match(IRPSPlayer player1, IRPSPlayer player2)
+	{
+		this.player1 = player1;
+		this.player2 = player2;
+		
+	}
+
+
+
+
+}
