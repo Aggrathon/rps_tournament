55429f146d5b9488ef5366a1e3b91cb0faec7563
Anton <antonbjo@gmail.com>
2015-10-01 22:35:46 +0300

    comments for the abstract player parent class

--- Assets/Scripts/Players/ARPSPlayer.cs
+++ Assets/Scripts/Players/ARPSPlayer.cs
@@ -17,17 +17,31 @@ public abstract class ARPSPlayer : ScriptableObject, IRPSPlayer {
 
 	public int health { get { return currentHealth; } set { currentHealth = value; } }
 
+	/// <summary>
+	/// This method sets up everything needed for a match
+	/// Call this version of the method FIRST if overwritten
+	/// </summary>
+	/// <param name="match"></param>
 	virtual public void startMatch(Match match)
 	{
 		currentMatch = match;
 		currentHealth = maxHealth;
 	}
 
+	/// <summary>
+	/// This method cleans up after a match is over
+	/// Call this version of the method LAST if overwritten
+	/// </summary>
 	virtual public void endMatch()
 	{
-		currentMatch.setPlayerFinished(this);
+		Match oldm = currentMatch;
 		currentMatch = null;
+		oldm.setPlayerFinished(this);
 	}
 
+	/// <summary>
+	/// This method informs the player that a choice/move is needed
+	/// The choice should then be registered with 'currentMatch.setPlayerChoice(this, choice);
+	/// </summary>
 	public abstract void newRound();
 }
