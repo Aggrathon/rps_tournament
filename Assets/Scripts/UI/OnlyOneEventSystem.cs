b82655941d9ee8a90b086ad6bfa3bf02d8928d00
Anton <antonbjo@gmail.com>
2015-10-10 16:25:36 +0300

    make script for only keeping one EventSystem in a scene

---
+++ Assets/Scripts/UI/OnlyOneEventSystem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnlyOneEventSystem : MonoBehaviour {
+	
+	void Awake () {
+		if(GameObject.FindObjectsOfType<EventSystem>().Length > 1)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
