b6ebb04736fd08146144180a0317ddb187a2eb07
Anton <antonbjo@gmail.com>
2015-10-12 20:28:48 +0300

    Create functionality for the menu

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public InputField playerName;
+
+	public void StartLadderTournament()
+	{
+		if(playerName.text != "")
+		{
+			TournamentState.instance.PlayerName = playerName.text;
+		}
+		Application.LoadLevel("tournament");
+	}
+}
