ecc6afd6dac9a57d9a19b2741439aac36b86107d
Anton <antonbjo@gmail.com>
2015-10-12 21:33:01 +0300

    fix issues in the battle scene

---
+++ Assets/Scripts/UI/OnlyInOneScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnlyInOneScene : MonoBehaviour {
+
+	public string sceneName;
+	
+	void Awake () {
+		if(Application.loadedLevelName.Equals(sceneName))
+		{
+			gameObject.SetActive(true);
+		}
+		else
+		{
+			gameObject.SetActive(false);
+			Destroy(gameObject);
+		}
+	}
+}
