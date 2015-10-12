dcfb8a471fb84b1c30d3324cd500e6209e6f1a72
Anton <antonbjo@gmail.com>
2015-10-12 20:50:02 +0300

    Add fade effect

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -5,6 +5,26 @@ using System.Collections;
 public class Menu : MonoBehaviour {
 
 	public InputField playerName;
+	public Image fader;
+	public float fadeInTime = 0.8f;
+    public float fadeOutTime = 0.3f;
+
+	IEnumerator Start()
+	{
+		Color c = Color.black;
+		c.a = 1f;
+		fader.color = c;
+		fader.gameObject.SetActive(true);
+		float time = 0f;
+		while(time< fadeInTime)
+		{
+			yield return new WaitForEndOfFrame();
+			time += Time.deltaTime;
+			c.a = (fadeInTime - time) / fadeInTime;
+			fader.color = c;
+		}
+		fader.gameObject.SetActive(false);
+	}
 
 	public void StartLadderTournament()
 	{
@@ -12,16 +32,33 @@ public class Menu : MonoBehaviour {
 		{
 			TournamentState.instance.PlayerName = playerName.text;
 		}
-		Application.LoadLevel("tournament");
+		StartCoroutine(ChangeScene("tournament"));
 	}
 
 	public void OpenMenu()
 	{
-		Application.LoadLevel("menu");
+		StartCoroutine(ChangeScene("menu"));
 	}
 
 	public void QuitApplication()
 	{
 		Application.Quit();
+	}
+
+	IEnumerator ChangeScene(string scene)
+	{
+		Color c = Color.black;
+		c.a = 0f;
+		fader.color = c;
+		fader.gameObject.SetActive(true);
+		float time = 0f;
+		while (time < fadeOutTime)
+		{
+			yield return new WaitForEndOfFrame();
+			time += Time.deltaTime;
+			c.a = time / fadeOutTime;
+			fader.color = c;
+		}
+		Application.LoadLevel(scene);
 	}
 }
