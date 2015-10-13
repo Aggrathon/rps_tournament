12e32494d24bbef0e02e71edb0e19ee2317e08bc
Anton <antonbjo@gmail.com>
2015-10-13 23:34:12 +0300

    Fibonacci and Newton

---
+++ Assets/Scripts/Players/AIs/FibonacciAI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FibonacciAI : ARPSPlayer {
+
+	private int num1, num2, count;
+	private System.Random rnd = new System.Random();
+	private Match.RPS curr = Match.RPS.Rock;
+
+	public override void startMatch(Match match)
+	{
+		base.startMatch(match);
+		num1 = 1;
+		num2 = 0;
+		count = 0;
+	}
+
+	public override void newRound()
+	{
+		currentMatch.setPlayerChoice(this, curr);
+		count++;
+		if(count >= num1)
+		{
+			count = num1;
+			num1 = num1 + num2;
+			num2 = count;
+			count = 0;
+			curr = (Match.RPS)(rnd.Next() % 3);
+		}
+	}
+}
